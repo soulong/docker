5f4207261216a99ff173deadb87798341a1d248c
Hao He <haohe90@gmail.com>
2020-07-11 18:16:08 +0800

    change to pipe mode

---
+++ install_docker_ubuntu_18_bionic.sh
@@ -0,0 +1,27 @@
+
+
+# remove old version of docker
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# install docker
+sudo apt-get update && \
+	sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+	sudo apt-key fingerprint 0EBFCD88 && \
+	sudo add-apt-repository "deb https://download.docker.com/linux/ubuntu bionic stable" &&\
+	sudo apt-get update && \
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
